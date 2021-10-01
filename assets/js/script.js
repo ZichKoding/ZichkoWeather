58df2723e8c264fccc79a84c9b80c2dbf59b9ccd
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-30 21:54:49 -0400

    Able to add weather data into the current slot and the 5 day forecasts and working on localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,53 @@
 // variables
 let myAPIWeatherKey = "&units=imperial&APPID=a15f02adbde7a78309af009d972b4360";
 let userSearch = "";
+let storeKey = 0;
+
+// all ids for the 5 day forecast
+let dayOneItems = [
+    "#dayOneDate",
+    "#dayOneIcon",
+    "#dayOneTemp",
+    "#dayOneWind",
+    "#dayOneHumid",
+    "#dayOneUV"
+];
+
+let dayTwoItems = [
+    "#dayTwoDate",
+    "#dayTwoIcon",
+    "#dayTwoTemp",
+    "#dayTwoWind",
+    "#dayTwoHumid",
+    "#dayTwoUV"
+];
+
+let dayThreeItems = [
+    "#dayThreeDate",
+    "#dayThreeIcon",
+    "#dayThreeTemp",
+    "#dayThreeWind",
+    "#dayThreeHumid",
+    "#dayThreeUV"
+];
+
+let dayFourItems = [
+    "#dayFourDate",
+    "#dayFourIcon",
+    "#dayFourTemp",
+    "#dayFourWind",
+    "#dayFourHumid",
+    "#dayFourUV"
+];
+
+let dayFiveItems = [
+    "#dayFiveDate",
+    "#dayFiveIcon",
+    "#dayFiveTemp",
+    "#dayFiveWind",
+    "#dayFiveHumid",
+    "#dayFiveUV"
+];
 
 // fetching data from Open Weather Map based off city or zipcode
 function makingWeatherCall(weather) {
@@ -8,10 +55,6 @@ function makingWeatherCall(weather) {
     .then(response => response.json())
     .then(function(data) {
         console.log(data);
-
-        // Adding icon to represent current weather
-        let curIcon = usingIcon(data.weather[0].icon);
-        $("#curIcon").attr("src", curIcon);
         
         // Pull the lattitude and longitude and use that to make a onecall
         // to openweathermap to get current and future forecasts.
@@ -36,22 +79,226 @@ function makingWeatherCall(weather) {
             // Display current UV index
             let curUV = Math.round(data.current.uvi);
             $("#curDayUV").text(curUV);
+
+            // for loop through index 0 - 4
+            for (let i = 0; i < 5; i++) {
+                let dailyIcon = data.daily[i].weather[0].icon;
+                let dailyTemp = data.daily[i].temp.day;
+                let dailyWind = data.daily[i].wind_gust;
+                let dailyHumid = data.daily[i].humidity;
+                let dailyUV = data.daily[i].uvi
+
+                switch(i) {
+                    // loading all data for day 1
+                    case 0:
+                        for (let x = 0; x < dayOneItems.length; x++) {
+                            switch(x) {
+                                case 0:
+                                    $(dayOneItems[x]).text(futureDates(i + 1));
+                                    break;
+                                case 1:
+                                    $(dayOneItems[x]).attr("src", usingIcon(dailyIcon));
+                                    break;
+                                case 2:
+                                    $(dayOneItems[x]).text(dailyTemp + "*F");
+                                    break;
+                                case 3:
+                                    $(dayOneItems[x]).text(dailyWind + " MPH");
+                                    break;
+                                case 4:
+                                    $(dayOneItems[x]).text(dailyHumid + "%");
+                                    break;
+                                case 5:
+                                    $(dayOneItems[x]).text(dailyUV);
+                                    break;
+                                default:
+                                    console.log("Obviously you are an idiot... or this needs to go to next day's data");
+                            }
+                        }
+                        break;
+
+                    // loading all data for day 2
+                    case 1:
+                        for (let x = 0; x < dayTwoItems.length; x++) {
+                            switch(x) {
+                                case 0:
+                                    $(dayTwoItems[x]).text(futureDates(i + 1));
+                                    break;
+                                case 1:
+                                    $(dayTwoItems[x]).attr("src", usingIcon(dailyIcon));
+                                    break;
+                                case 2:
+                                    $(dayTwoItems[x]).text(dailyTemp + "*F");
+                                    break;
+                                case 3:
+                                    $(dayTwoItems[x]).text(dailyWind + " MPH");
+                                    break;
+                                case 4:
+                                    $(dayTwoItems[x]).text(dailyHumid + "%");
+                                    break;
+                                case 5:
+                                    $(dayTwoItems[x]).text(dailyUV);
+                                    break;
+                                default:
+                                    console.log("Obviously you are an idiot... or this needs to go to next day's data");
+                            }
+                        }
+                        break;
+
+                    // loading all data for day 3                        
+                    case 2:
+                        for (let x = 0; x < dayThreeItems.length; x++) {
+                            switch(x) {
+                                case 0:
+                                    $(dayThreeItems[x]).text(futureDates(i + 1));
+                                    break;
+                                case 1:
+                                    $(dayThreeItems[x]).attr("src", usingIcon(dailyIcon));
+                                    break;
+                                case 2:
+                                    $(dayThreeItems[x]).text(dailyTemp + "*F");
+                                    break;
+                                case 3:
+                                    $(dayThreeItems[x]).text(dailyWind + "MPH");
+                                    break;
+                                case 4:
+                                    $(dayThreeItems[x]).text(dailyHumid + "%");
+                                    break;
+                                case 5:
+                                    $(dayThreeItems[x]).text(dailyUV);
+                                    break;
+                                default:
+                                    console.log("Obviously you are an idiot... or this needs to go to next day's data");
+                            }
+                        }
+                        break;
+
+                    // loading all data for day 4
+                    case 3:
+                        for (let x = 0; x < dayFourItems.length; x++) {
+                            switch(x) {
+                                case 0:
+                                    $(dayFourItems[x]).text(futureDates(i + 1));
+                                    break;
+                                case 1:
+                                    $(dayFourItems[x]).attr("src", usingIcon(dailyIcon));
+                                    break;
+                                case 2:
+                                    $(dayFourItems[x]).text(dailyTemp + "*F");
+                                    break;
+                                case 3:
+                                    $(dayFourItems[x]).text(dailyWind + " MPH");
+                                    break;
+                                case 4:
+                                    $(dayFourItems[x]).text(dailyHumid + "%");
+                                    break;
+                                case 5:
+                                    $(dayFourItems[x]).text(dailyUV);
+                                    break;
+                                default:
+                                    console.log("Obviously you are an idiot... or this needs to go to next day's data");
+                            }
+                        }
+                        break;
+                    // loading data for day 5
+                    case 4:
+                        for (let x = 0; x < dayFiveItems.length; x++) {
+                            switch(x) {
+                                case 0:
+                                    $(dayFiveItems[x]).text(futureDates(i + 1));
+                                    break;
+                                case 1:
+                                    $(dayFiveItems[x]).attr("src", usingIcon(dailyIcon));
+                                    break;
+                                case 2:
+                                    $(dayFiveItems[x]).text(dailyTemp + "*F");
+                                    break;
+                                case 3:
+                                    $(dayFiveItems[x]).text(dailyWind + " MPH");
+                                    break;
+                                case 4:
+                                    $(dayFiveItems[x]).text(dailyHumid + "%");
+                                    break;
+                                case 5:
+                                    $(dayFiveItems[x]).text(dailyUV);
+                                    break;
+                                default:
+                                    console.log("Obviously you are an idiot... or this needs to go to next day's data");
+                            }
+                        }
+                        break;
+                }
+            }
         });
+
+        // Reverse geocoding to pull the city and state and display that in the current weather
+        let reversed = reverseGeo(data.coord.lat, data.coord.lon);
+        fetch(reversed)
+        .then(response => response.json())
+        .then(function(data) {
+            console.log(data);
+            let city = data[0].name;
+            let state = data[0].state;
+            $("#currentCityDate").text("");
+            $("#currentCityDate").text(city + ", " + state + " " + moment().format("MM/DD/YYYY"));
+        });
+
+        
+        // Adding icon to represent current weather
+        let curIcon = usingIcon(data.weather[0].icon);
+        $("#curIcon").attr("src", curIcon);
     });
 };
+
+// a function to keep from writing out the link everytime.
 function usingIcon(icon) {
     let iconPath = "https://openweathermap.org/img/w/" + icon + ".png";
     return iconPath;
 };
 
+// a function to keep from writing out the link everytime.
 function fetchOneCall(lat, lon) {
     let ppfWeatherData = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + myAPIWeatherKey;
     return ppfWeatherData;
 };
 
+// a function to keep from writing out the link everytime.
+function reverseGeo(lat, lon) {
+    let reversing = "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=1&appid=a15f02adbde7a78309af009d972b4360";
+    return reversing;
+}
 
+// a function for automating the dates for the 5 day forecast $(dayOneItems[x]).text(moment().add(1, "d"))
+function futureDates(nthDays) {
+    let xDays = moment().add(nthDays, "d").format("MM/DD/YYYY");
+    return xDays;
+}
+
+
+// get last search before page was refreshed and display that data
+function previData() {
+    if (localStorage.length > 0){
+        userSearch = localStorage.getItem(localStorage.length - 1 );
+        console.log(userSearch);
+        let curWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearch + myAPIWeatherKey;
+        console.log(curWeatherAPI);
+        makingWeatherCall(curWeatherAPI);
+    }
+    else {
+        console.log("There is no recent search history");
+    }
+};
+
+previData();
+
+// User search data and displaying the data they are requesting
 $(".searchBtn").click(function() {
     userSearch = $("#search").val();
     let curWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearch + myAPIWeatherKey;
     makingWeatherCall(curWeatherAPI);
+
+    // setting search value in local storage
+    localStorage.setItem(storeKey, JSON.stringify(userSearch));
+    storeKey++;
 });
+
